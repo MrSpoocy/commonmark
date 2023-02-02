9c9c2dd0cf1ba4caf8f53e4e4dc9259578c6146d
MrSpoocy <github@mrspoocy.com>
2023-02-02 16:30:59 +0100

    add table colspan/rowspan tests

---
+++ tests/unit/Extension/ExtendedTable/TableColspanRendererTest.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace League\CommonMark\Tests\Unit\Extension\ExtendedTable;
+
+use League\CommonMark\Environment\Environment;
+use League\CommonMark\Event\DocumentParsedEvent;
+use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
+use League\CommonMark\Extension\ExtendedTable\ExtendedTableProcessor;
+use League\CommonMark\Extension\GithubFlavoredMarkdownExtension;
+use League\CommonMark\Parser\MarkdownParser;
+use League\CommonMark\Renderer\HtmlRenderer;
+use PHPUnit\Framework\TestCase;
+
+class TableColspanRendererTest extends TestCase
+{
+
+    public function testSingleColspan(): void {
+        $string = <<<TABLE
+| Headline 1 | Headline 2  | Headline 3 |
+|------------|-------------|------------|
+| first cell || colspan cell            |
+| last cell  | last cell   |            |
+TABLE;
+
+        $expected = <<<HTML
+<table>
+<thead>
+<tr>
+<th>Headline 1</th>
+<th>Headline 2</th>
+<th>Headline 3</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>first cell</td>
+<td colspan="2">colspan cell</td>
+</tr>
+<tr>
+<td>last cell</td>
+<td>last cell</td>
+<td></td>
+</tr>
+</tbody>
+</table>
+
+HTML;
+
+        $environment = new Environment();
+        $environment->addExtension(new CommonMarkCoreExtension());
+        $environment->addExtension(new GithubFlavoredMarkdownExtension());
+
+        $environment->addEventListener(DocumentParsedEvent::class, new ExtendedTableProcessor());
+
+
+        $parser   = new MarkdownParser($environment);
+        $renderer = new HtmlRenderer($environment);
+
+        $document = $parser->parse($string);
+
+        $html = (string) $renderer->renderDocument($document);
+
+        $this->assertSame($expected, $html);
+    }
+
+    public function testMultipleColspan(): void {
+        $string = <<<TABLE
+| Headline 1 | Headline 2  | Headline 3 | Headline 4 |
+|------------|-------------|------------|------------|
+| first cell ||| colspan cell                        |
+| last cell  | last cell   |            |            |
+|| colspan cell            |            |            |
+TABLE;
+
+        $expected = <<<HTML
+<table>
+<thead>
+<tr>
+<th>Headline 1</th>
+<th>Headline 2</th>
+<th>Headline 3</th>
+<th>Headline 4</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>first cell</td>
+<td colspan="3">colspan cell</td>
+</tr>
+<tr>
+<td>last cell</td>
+<td>last cell</td>
+<td></td>
+<td></td>
+</tr>
+<tr>
+<td colspan="2">colspan cell</td>
+<td></td>
+<td></td>
+</tr>
+</tbody>
+</table>
+
+HTML;
+
+        $environment = new Environment();
+        $environment->addExtension(new CommonMarkCoreExtension());
+        $environment->addExtension(new GithubFlavoredMarkdownExtension());
+
+        $environment->addEventListener(DocumentParsedEvent::class, new ExtendedTableProcessor());
+
+
+        $parser   = new MarkdownParser($environment);
+        $renderer = new HtmlRenderer($environment);
+
+        $document = $parser->parse($string);
+
+        $html = (string) $renderer->renderDocument($document);
+
+        $this->assertSame($expected, $html);
+    }
+}
---
+++ tests/unit/Extension/ExtendedTable/TableRowspanRendererTest.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace League\CommonMark\Tests\Unit\Extension\ExtendedTable;
+
+use League\CommonMark\Environment\Environment;
+use League\CommonMark\Event\DocumentParsedEvent;
+use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
+use League\CommonMark\Extension\ExtendedTable\ExtendedTableProcessor;
+use League\CommonMark\Extension\GithubFlavoredMarkdownExtension;
+use League\CommonMark\Parser\MarkdownParser;
+use League\CommonMark\Renderer\HtmlRenderer;
+use PHPUnit\Framework\TestCase;
+
+class TableRowspanRendererTest extends TestCase
+{
+    public function testSingleRowspan(): void {
+        $string = <<<TABLE
+| Headline 1 | Headline 2  | Headline 3 |
+|------------|-------------|------------|
+| first cell | cell 2      | cell 3     |
+|^           | cell 5      |            |
+|^           | cell 8      | cell 9     |
+TABLE;
+
+        $expected = <<<HTML
+<table>
+<thead>
+<tr>
+<th>Headline 1</th>
+<th>Headline 2</th>
+<th>Headline 3</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td rowspan="3">first cell</td>
+<td>cell 2</td>
+<td>cell 3</td>
+</tr>
+<tr>
+<td>cell 5</td>
+<td></td>
+</tr>
+<tr>
+<td>cell 8</td>
+<td>cell 9</td>
+</tr>
+</tbody>
+</table>
+
+HTML;
+
+        $environment = new Environment();
+        $environment->addExtension(new CommonMarkCoreExtension());
+        $environment->addExtension(new GithubFlavoredMarkdownExtension());
+
+        $environment->addEventListener(DocumentParsedEvent::class, new ExtendedTableProcessor());
+
+
+        $parser   = new MarkdownParser($environment);
+        $renderer = new HtmlRenderer($environment);
+
+        $document = $parser->parse($string);
+
+        $html = (string) $renderer->renderDocument($document);
+
+        $this->assertSame($expected, $html);
+    }
+
+    public function testMultipleRowspan(): void {
+        $string = <<<TABLE
+| Headline 1 | Headline 2  | Headline 3 |
+|------------|-------------|------------|
+| first cell | cell 2      | cell 3     |
+|^           | cell 5      | ^          |
+|^           | cell 8      | ^          |
+TABLE;
+
+        $expected = <<<HTML
+<table>
+<thead>
+<tr>
+<th>Headline 1</th>
+<th>Headline 2</th>
+<th>Headline 3</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td rowspan="3">first cell</td>
+<td>cell 2</td>
+<td rowspan="3">cell 3</td>
+</tr>
+<tr>
+<td>cell 5</td>
+</tr>
+<tr>
+<td>cell 8</td>
+</tr>
+</tbody>
+</table>
+
+HTML;
+
+        $environment = new Environment();
+        $environment->addExtension(new CommonMarkCoreExtension());
+        $environment->addExtension(new GithubFlavoredMarkdownExtension());
+
+        $environment->addEventListener(DocumentParsedEvent::class, new ExtendedTableProcessor());
+
+
+        $parser   = new MarkdownParser($environment);
+        $renderer = new HtmlRenderer($environment);
+
+        $document = $parser->parse($string);
+
+        $html = (string) $renderer->renderDocument($document);
+
+        $this->assertSame($expected, $html);
+    }
+}
